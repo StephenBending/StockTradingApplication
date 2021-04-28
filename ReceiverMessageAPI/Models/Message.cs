82318212b77cb7063708d97ac18b24260a71d504
Sam <husam_ali07@hotmail.com>
2021-04-28 17:51:08 +0100

    Message-receiver section comlplete (1st commit)

---
+++ ReceiverMessageAPI/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReceiverMessageAPI.Models
+{
+    public class Message
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double value { get; set; }
+    }
+}
---
+++ ReceiverMessageAPI/Services/RabbitService.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiverMessageAPI.Services
+{
+    public class RabbitService : IRabbitService
+    {
+
+        public string CollectMessage(string queueName)
+        {
+            var receivedMessage = string.Empty;
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "stock", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                // below is how to consume the message
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    // WHY USE SPAN?? because rabbitMQ did not update whn they updated the whole thing
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Recieved {0}", message);
+                    //
+                    receivedMessage = message;
+                };
+                channel.BasicConsume(queue: "stock", autoAck: true, consumer: consumer);
+
+
+            }
+            return receivedMessage;
+        }
+    }
+}
