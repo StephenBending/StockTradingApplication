c30a9e8547ccae167c1842fa24a8c6b67a99d05f
GianlucaF99 <gianlucaf1999@hotmail.co.uk>
2021-04-28 16:18:09 +0100

    Presentation sender should be done, if there are any problems with it let me know and ill try and fix them

---
+++ SenderPresentation/Context/UpdateContext.cs
@@ -0,0 +1,15 @@
+﻿using SenderPresentation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenderPresentation.Context
+{
+    public class UpdateContext : DbContext
+    {
+        public DbSet<Update> StockTableSender { get; set; }
+    }
+
+}
