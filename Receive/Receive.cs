1fb44b15f32b3324b481bea0e9a4b70011aa1150
Stephen <stephenbending@hotmail.co.uk>
2021-04-29 09:19:49 +0100

    Merged Waqas's work

---
+++ Receive/Receive.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections;
+using System.Text;
+
+namespace Receive
+{
+    class Receive
+    {
+        public ArrayList receiveMessages()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            var messageList = new ArrayList();
+            
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+
+            channel.QueueDeclare(
+                queue: "stock",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                //Console.WriteLine("Receiver received {0}", message);
+                messageList.Add(message);
+            };
+
+            channel.BasicConsume(
+                queue: "stock",
+                autoAck: true,
+                consumer: consumer);
+
+            //Console.ReadLine();
+            return messageList;
+        }
+    }
+}
---
+++ Send/Send.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Send
+    {
+        private readonly ConnectionFactory _factory;
+
+        public Send(ConnectionFactory factory)
+        {
+            _factory = factory;
+        }
+
+        public void setupSend()
+        {
+            // Test Data
+            Stock stock = new Stock
+            {
+                Id = 1,
+                Name = "Google",
+                Value = 1234
+            };
+
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                queue: "stock",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            /*
+            string message = JsonConvert.SerializeObject(stock);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "stock",
+                basicProperties: null,
+                body: body);
+
+            Console.WriteLine("Sender sent {0}", message);
+            Console.ReadLine();
+            */
+        }
+
+        public void send(string message)
+        {
+            using var connection = _factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "stock",
+                basicProperties: null,
+                body: body);
+        }
+    }
+}
